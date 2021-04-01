64ad907cd07d037ffb2e674661714b719914da13
HatamJafarov <hatam.jafarov@gmail.com>
2021-04-02 00:15:50 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let loader = document.querySelector(".loader");
 const song = document.querySelector(".song");
 const play = document.querySelector(".play");
 const replay = document.querySelector(".replay");
@@ -12,56 +13,63 @@ const outlineLength = outline.getTotalLength();
 const timeSelect = document.querySelectorAll(".time-select button");
 let fakeDuration = 120;
 
+window.onload = () => {
+    loader.style.display = "block";
+    if (document.readyState === "complete") {
+        loader.style.display = "none";
+    }
+};
+
 outline.style.strokeDasharray = outlineLength;
 outline.style.strokeDashoffset = outlineLength;
 
 play.addEventListener("click", () => {
-  checkPlaying(song);
+    checkPlaying(song);
 });
 
 timeSelect.forEach((e) => {
-  e.addEventListener("click", function () {
-    fakeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)} : ${Math.floor(
-      fakeDuration % 60
-    )}`;
-  });
+    e.addEventListener("click", function () {
+        fakeDuration = this.getAttribute("data-time");
+        timeDisplay.textContent = `${Math.floor(
+            fakeDuration / 60
+        )} : ${Math.floor(fakeDuration % 60)}`;
+    });
 });
 
 sounds.forEach((sound) => {
-  sound.addEventListener("click", function () {
-    song.src = this.getAttribute("data-sound");
-    video.src = this.getAttribute("data-video");
-    checkPlaying(song);
-  });
+    sound.addEventListener("click", function () {
+        song.src = this.getAttribute("data-sound");
+        video.src = this.getAttribute("data-video");
+        checkPlaying(song);
+    });
 });
 
 const checkPlaying = (song) => {
-  if (song.paused) {
-    song.play();
-    video.play();
-    play.src = "./svg/pause.svg";
-  } else {
-    song.pause();
-    video.pause();
-    play.src = "./svg/play.svg";
-  }
+    if (song.paused) {
+        song.play();
+        video.play();
+        play.src = "./svg/pause.svg";
+    } else {
+        song.pause();
+        video.pause();
+        play.src = "./svg/play.svg";
+    }
 };
 
 song.ontimeupdate = () => {
-  let currentTime = song.currentTime;
-  let elapsed = fakeDuration - currentTime;
-  let seconds = Math.floor(elapsed % 60);
-  let minutes = Math.floor(elapsed / 60);
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
 
-  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-  outline.style.strokeDashoffset = progress;
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
 
-  timeDisplay.textContent = `${minutes}:${seconds}`;
+    timeDisplay.textContent = `${minutes}:${seconds}`;
 
-  if (currentTime >= fakeDuration) {
-    song.pause();
-    song.currentTime = 0;
-    play.src = "./svg/play.svg";
-  }
+    if (currentTime >= fakeDuration) {
+        song.pause();
+        song.currentTime = 0;
+        play.src = "./svg/play.svg";
+    }
 };
